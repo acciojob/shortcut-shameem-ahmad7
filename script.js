61b2fd480adfb9169df78494c1b558db547806eb
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-21 12:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	if (str1.length > 0 && str2.length > 0) {
+        // Get the first letters of both strings
+        const firstLetterStr1 = str1.charAt(0);
+        const firstLetterStr2 = str2.charAt(0);
+        
+        // Concatenate and return them
+        return firstLetterStr1 + firstLetterStr2;
+    }
+    
+    // Return an empty string if any input is empty
+    return '';
 }
 
 // Do not change the code below.
